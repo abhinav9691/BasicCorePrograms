098db0f34d0e084e2b236c6e7c0721c5314dd7c7
Abhinav Kumar <kabhinav713@gmail.com>
2023-06-22 09:08:09 +0530

    [Abhinav]-adding Flip Coin

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using BasicCorePrograms1;
+
+while (true)
+{
+    Console.WriteLine("Enter the Choice \n 1)Filp Coin");
+    int choice = Convert.ToInt32(Console.ReadLine());
+    switch (choice)
+    {
+        case 1:
+            FlipCoin flipcoin = new FlipCoin();
+            flipcoin.HeadTails();
+            break;
+    }
+}
