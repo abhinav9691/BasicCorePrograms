b4fda6e020abdd9fabf05e7a35b88b0ab2d3ec13
Abhinav Kumar <kabhinav713@gmail.com>
2023-06-22 09:36:48 +0530

    [Abhinav]-adding QandR

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 while (true)
 {
     Console.WriteLine("Enter the Choice \n 1)Filp Coin\n2)Leap Year\n3)Power Of 2\n" +
-        "4)Harmonic Number\n5)Prime Factor");
+        "4)Harmonic Number\n5)Prime Factor\n6)Quotient and Reminder\n7)Swap Two Numbers");
     int choice = Convert.ToInt32(Console.ReadLine());
     if (choice != 0)
     {
@@ -29,10 +29,12 @@ while (true)
                 PrimeFactors primeFactor = new PrimeFactors();
                 primeFactor.Factor();
                 break;
+            case 6:
+                QuotientRemainder quotientRemainder = new QuotientRemainder();
+                quotientRemainder.QandR();
+                break;
+            
         }
     }
-    else
-    {
-        Console.WriteLine("Enter the Correct Choice");
-    }
+    
 }
